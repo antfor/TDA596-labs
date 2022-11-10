43052fc0d53b3f1f7a7abc4d2bf5abf20c0d49ef
antfor <38752136+antfor@users.noreply.github.com>
2022-11-10 21:39:42 +0100

    it works?

--- lab1/http_server.go
+++ lab1/http_server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
-	"log"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 
 	"golang.org/x/sync/semaphore"
 )
@@ -55,12 +57,23 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	case "POST":
 		handlePost(w, req)
 	default:
-		w.WriteHeader(501)
+		http.Error(w, "501 Not Implemented", http.StatusNotImplemented)
 	}
+}
+
+func getPath(w http.ResponseWriter, req *http.Request) {
+
 }
 
 func handleGet(w http.ResponseWriter, req *http.Request) {
 
+	//path, format, err := getPath(w, req)
+
+	if true {
+		//http.Error(w, "404 not found.", http.StatusNotFound)
+	} else {
+		//http.ServeFile(w, req, path)
+	}
 }
 
 func handlePost(w http.ResponseWriter, req *http.Request) {
@@ -69,7 +82,65 @@ func handlePost(w http.ResponseWriter, req *http.Request) {
 
 func listen() {
 
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
+	//log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
+
+	l, _ := net.Listen("tcp", ":"+strconv.Itoa(port))
+
+	// error handliing
+
+	defer l.Close()
+
+	host, hostPort, _ := net.SplitHostPort(l.Addr().String())
+
+	// error handliing
+
+	fmt.Printf("Listening on host: %s, port: %s\n", host, hostPort)
+
+	for {
+
+		conn, _ := l.Accept()
+		fmt.Println("accpeted")
+		//error handling
+		sem.Acquire(ctx, 1)
+		go handleConnection(conn)
+
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	defer sem.Release(1)
+
+	request(conn)
+	response(conn)
+
+}
+
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if i == 0 {
+			m := strings.Fields(line)[0]
+			fmt.Println("Methods", m)
+		}
+		if line == "" {
+			break
+		}
+		i++
+	}
+
+}
+func response(conn net.Conn) {
+	body := "This Is Go Http Server Using TCP"
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
 }
 
 func getPort() bool {
