840bcadb5bceb63c83e0ddfc494ca954297a0840
antfor <38752136+antfor@users.noreply.github.com>
2022-12-01 15:05:35 +0100

    lab2 p2p 📞

---
+++ lab2/argument/argument.go
@@ -0,0 +1,76 @@
+package argument
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Argument struct {
+	A   string
+	p   int
+	Ja  string
+	Jp  int
+	Ts  int
+	Tff int
+	Tcp int
+	R   int
+	I   string
+}
+
+const (
+	Create = 0
+	Join   = 1
+)
+
+func NewArg() (Argument, int) {
+
+	arg := Argument{}
+
+	for i := 1; i < len(os.Args); i += 2 {
+		fmt.Println(i, os.Args[i])
+
+		switch os.Args[i] {
+		case "-a":
+			arg.A = os.Args[i+1]
+		case "-p":
+			arg.p = getPort(os.Args[i+1])
+		case "--ja":
+			arg.Ja = os.Args[i+1]
+		case "--jp":
+			arg.Jp = getPort(os.Args[i+1])
+		case "--ts":
+			arg.Ts, _ = strconv.Atoi(os.Args[i+1]) // todo fix 1-60000
+		case "--tff":
+			arg.Tff, _ = strconv.Atoi(os.Args[i+1]) // todo fix 1-60000
+		case "--tcp":
+			arg.Tcp, _ = strconv.Atoi(os.Args[i+1]) // todo fix 1-60000
+		case "-r":
+			arg.R, _ = strconv.Atoi(os.Args[i+1]) // todo fix 1-32
+		case "-i":
+			arg.I = os.Args[i+1] // todo fix string of 40 characters matching [0-9a-fA-F]
+
+		}
+	}
+
+	//validArgs() TODO
+
+	if arg.Ja == "" && arg.Jp == 0 {
+		return arg, Create
+	} else if arg.Ja != "" && arg.Jp != 0 {
+		return arg, Join
+	}
+
+	return arg, -1
+}
+
+func getPort(sPort string) int {
+
+	portNum, err := strconv.Atoi(sPort)
+
+	if err != nil || portNum < 0 || portNum > 65535 {
+		panic("Error in argument: not a valid port")
+	}
+
+	return portNum
+}
---
+++ lab2/chord.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"lab2/argument"
+	"math/big"
+)
+
+type Key string
+
+//type IPaddress string
+
+type node struct {
+	ip   string
+	port int
+	key  *big.Int
+
+	keys_to_string map[Key]string
+
+	fingers     []string
+	predecessor string
+	successors  []string
+}
+
+func hash(elt string) *big.Int {
+	hasher := sha1.New()
+	hasher.Write([]byte(elt))
+	return new(big.Int).SetBytes(hasher.Sum(nil))
+}
+
+func main() {
+	arg, argType := argument.NewArg()
+	fmt.Println(arg, "hej")
+
+	if argType == argument.Create {
+		n := node{}
+
+		n.createRing()
+
+	} else if argType == argument.Join {
+
+		n := node{ip: arg.Ja, port: arg.Jp}
+
+		n.joinRing(n)
+	} else {
+		panic("invalid arguments")
+	}
+
+}
+
+func (n node) createRing() {
+	fmt.Println("CREATE")
+	n.predecessor = ""
+	n.successors = append(n.successors, n.ip)
+}
+
+func (n node) joinRing(n2 node) {
+	fmt.Println("JOIN")
+	n.predecessor = ""
+	n.successors = append(n.successors, n2.findSuccessor(n))
+}
+
+func lookUp() {
+
+}
+
+func (n node) findSuccessor(n2 node) node {
+
+	return node{}
+}
+
+func (n node) closest_preceding_node() {
+
+}
+
+// Stabilze stuff
+func stabilze() {
+
+}
+
+func notify() {
+
+}
+
+func fix_fingers() {
+
+}
+
+func check_predessesor() {
+
+}
+
+func storeFile() {
+
+}
+
+func printState() {
+
+}
